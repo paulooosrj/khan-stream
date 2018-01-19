08342bd1448c625db4183e4a4a3a4d23bcc251f1
Paulao Developer <jskhanframework@gmail.com>
2018-01-19 10:00:54 -0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var getFilmes = require("../get_filmes");
 
 /* GET films listing. */
 router.get('/', function(req, res, next) {
     
     res.render('index');
-
-});
-
-router.get('/api/:id?*', async function(req, res, next){
-  
-    var id = req.params.id || 1;
-    let films = await getFilmes(id);
-    res.send(films);
 
 });
 
